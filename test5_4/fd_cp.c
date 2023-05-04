bf7c16ac3df923dd76626e3904d4ed99518aa2a6
Libra20000831@gmail.com <75510639+Liw-die@users.noreply.github.com>
2023-05-04 17:23:05 +0800

    文件IO

--- test5_4/fd_cp.c
+++ test5_4/fd_cp.c
@@ -5,13 +5,25 @@
 #include <fcntl.h>
 #include <string.h>
 #include <sys/mman.h>
-
+#define SIZ 1024
 
 void file_cp(int fd_source,int fd_bourn)
 {
+	char buf[SIZ];
+	bzero(buf,SIZ);
+	int ret;
+	ret=read(fd_source,buf,SIZ);
+	write(fd_bourn,buf,strlen(buf));
+	printf("ret=%d write->%s \n",ret,buf);
 }
 
 int main(void)
 {
 	int fd_source=open("3.txt",O_RDONLY);
 	int fd_bourn=open("4.txt",O_RDWR);
+	file_cp(fd_source,fd_bourn);
+	close(fd_source);
+	close(fd_bourn);
+	
+	return 0;
+}
