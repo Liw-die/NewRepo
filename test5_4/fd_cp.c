ccd6c27a1af9b1337b55eb6c3e211756b31a5c57
Libra20000831@gmail.com <75510639+Liw-die@users.noreply.github.com>
2023-05-04 19:28:07 +0800

    文件IO

--- test5_4/fd_cp.c
+++ test5_4/fd_cp.c
@@ -5,25 +5,67 @@
 #include <fcntl.h>
 #include <string.h>
 #include <sys/mman.h>
+
 #define SIZ 1024
 
-void file_cp(int fd_source,int fd_bourn)
+void file_cp(const char *source_filename,const char *bourn_filename)
 {
-	char buf[SIZ];
-	bzero(buf,SIZ);
-	int ret;
-	ret=read(fd_source,buf,SIZ);
-	write(fd_bourn,buf,strlen(buf));
-	printf("ret=%d write->%s \n",ret,buf);
+	int fd_sou=open(source_filename,O_RDONLY);
+	if(fd_sou==-1)
+	{
+		printf("文件不存在,是否创建该文件0/1:");
+		int temp;
+		scanf("%d",&temp);
+		getchar();
+		if(temp==1)
+		{
+			fd_sou=open(source_filename,O_RDWR|O_CREAT,0777);
+			printf("%s文件创建成功\n",source_filename);
+		}
+		else
+			return ;
+	}
+	int fd_bou=open(bourn_filename,O_RDWR);
+	if(fd_bou==-1)
+	{
+		printf("文件不存在,是否创建该文件0/1:");
+		int temp;
+		scanf("%d",&temp);
+		getchar();
+		if(temp==1)
+		{
+			fd_bou=open(bourn_filename,O_RDWR|O_CREAT,0777);
+			printf("%s文件创建成功\n",bourn_filename);
+		}
+		else
+			return ;
+	}
+	int res;
+	char temp[SIZ];
+	bzero(temp,sizeof(temp));
+	lseek(fd_bou,0,SEEK_SET);
+	int i=0,te;
+	while((res=read(fd_sou,temp,SIZ))>0)
+	{
+		i++;
+		printf("res=%d写入完成->%d\n",res,i);
+		if((te=write(fd_bou,temp,SIZ))!=res)
+		{
+			printf("te=%d写入完成!\n",te);
+			close(fd_sou);
+			close(fd_bou);
+		}
+	}
 }
 
 int main(void)
 {
-	int fd_source=open("3.txt",O_RDONLY);
-	int fd_bourn=open("4.txt",O_RDWR);
-	file_cp(fd_source,fd_bourn);
-	close(fd_source);
-	close(fd_bourn);
-	
+	char sou_fil[15]= "ikun.txt";
+	char bou_fil[15]= "4.txt";
+	//bzero(sou_fil,sizeof(sou_fil));
+	//bzero(bou_fil,sizeof(bou_fil));
+	//printf("请输入两个文件的地址：");
+	//scanf("%s %s",sou_fil,bou_fil);
+	file_cp(sou_fil,bou_fil);
 	return 0;
 }
